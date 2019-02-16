fbccdf163d31d6f9fd8888396f838369eaf2da31
Juha Liias <juha.liias@gmail.com>
2019-02-16 15:57:03 +0200

    static files work too

--- westsloth/urls.py
+++ westsloth/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 import games.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', games.views.homepage, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 #    path('admin/', admin.site.urls),
